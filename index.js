cf795485ba8bc92eb9a67ee520318971f9cad875
howarde8 <howarde8@gmail.com>
2017-09-28 01:14:59 -0700

    Test ajax refresh successful

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send({ hi: "there" });
+    res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.get("/text-response", (req, res) => {
+    user = req.query["user"];
+    resText = "Hello " + user + "!";
+    res.send(resText);
 });
 
 app.listen(5000);
