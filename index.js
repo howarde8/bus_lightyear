ba7714c99741f902b5f1ced870f560d2a95f555c
howarde8 <howarde8@gmail.com>
2017-10-06 02:32:56 -0700

    Outh basic test

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
+const keys = require("./config/keys");
 const express = require("express");
 const path = require("path");
 const app = express();
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: "/auth/google/callback"
+        },
+        (accessToken, refreshToken, profile, done) => {
+            console.log('access Token: ',accessToken);
+            console.log('refresh token: ', refreshToken);
+            console.log('profile: ', profile);
+        }
+    )
+);
+
+app.get(
+    "/auth/google",
+    passport.authenticate("google", {
+        scope: ["profile", "email"]
+    })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 // define data
 var homepageJson = {
