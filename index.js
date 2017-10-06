aa17993d8614c61b05db6f5f0db00c1e78b979bf
howarde8 <howarde8@gmail.com>
2017-10-06 07:21:48 -0700

    Finish auth flow

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ var homepageJson = {
 };
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + "/index.html"));
+    res.sendFile(path.join(__dirname + "/html/index.html"));
 });
 
 app.get("/homepage-info", (req, res) => {
