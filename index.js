8f9c9b020edf099161b96e1255abde7708eb53df
howarde8 <howarde8@gmail.com>
2017-10-06 05:59:22 -0700

    Authentication test by return user info successfully

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
+
+
+// cookie session
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000, // should be microsecond
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 require("./routes/authRoutes")(app);
 
 // mongodb relatives
-const keys = require("./config/keys");
 mongoose.connect(keys.mongoURI);
 
 // define data
