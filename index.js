dbf327de3c72fb5d8af6efa7df031c07621d08be
howarde8 <howarde8@gmail.com>
2018-01-30 12:00:17 -0800

    Add routing rules index.js

--- index.js
+++ index.js
@@ -25,6 +25,19 @@ require("./routes/authRoutes")(app);
 require("./routes/dataRoutes")(app);
 // app.use(express.static('html'));
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('bus-react/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'bus-react', 'build', 'index.html'));
+  });
+}
+
 // mongodb relatives
 mongoose.connect(keys.mongoURI);
 
