aefe9079c514448508d1d029e83d3d053cc8a274
Louis <louis.hsiao@d8ai.com>
2018-02-13 09:57:24 +0800

    reducer

--- bus-react/src/reducers/products.js
+++ bus-react/src/reducers/products.js
@@ -2,7 +2,8 @@ const initialState = ["test"];
 
 const products = (state = initialState, action) => {
     switch(action.type){
-        case "SHOW_ALL":{
+        case "INIT_ITEMS":{
+            state = action.items;
             return state;
         }
         default:
