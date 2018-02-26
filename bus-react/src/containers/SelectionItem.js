3b6e9186167118bc9098de7adb913e2d56d8dffb
Louis <pohsiu0709@gmail.com>
2018-02-27 07:21:15 +0800

    with datepicker

--- bus-react/src/containers/SelectionItem.js
+++ bus-react/src/containers/SelectionItem.js
@@ -1,9 +1,19 @@
-
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import {setVisibilityFilter} from '../actions/item';
+import { DateRangePicker, SingleDatePicker, DayPickerRangeController } from 'react-dates';
 
 class SelectionItem extends React.Component{
+    constructor(props){
+      super(props);
+      this.state ={
+        startDate:null,
+        endDate:null,
+        focusedInput:null,
+      }
+    }
     numberFilter(low,up){
       this.props.dispatch(setVisibilityFilter({filterType:"FILT_NUMBER",lowBound:low,upBound:up}));
     }
@@ -17,7 +27,17 @@ class SelectionItem extends React.Component{
       return(
         <div class="select_bar">
           <div class="selcet_btns">
-            <div class="btn">日期</div>
+            <div style={{zIndex:9999}}> 
+            <DateRangePicker
+              startDate={this.state.startDate} // momentPropTypes.momentObj or null,
+              startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
+              endDate={this.state.endDate} // momentPropTypes.momentObj or null,
+              endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
+              onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
+              focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+              onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+            />
+            </div>
             <div class="btn" onClick={()=>this.showAll()}>顯示全部</div>
             <li class="navbar-header nav navbar-navdropdown">
               <div class="dropdown-toggle btn" data-toggle="dropdown">人數限制</div>
@@ -38,8 +58,8 @@ class SelectionItem extends React.Component{
             <li class="navbar-header nav navbar-navdropdown">
               <div class="dropdown-toggle btn" data-toggle="dropdown">依「評價」由高至低排序</div>
               <ul class="dropdown-menu">
-                <li><a>由高至低</a></li>
-                <li><a>由低至高</a></li>
+                <li><a>依「價格」由高至低</a></li>
+                <li><a>依「價格」由低至高</a></li>
               </ul>
             </li>
             
