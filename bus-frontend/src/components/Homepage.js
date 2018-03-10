9f1fec3f7d4fe9925c9debf4062b6ffb41b72501
howarde8 <howarde8@gmail.com>
2018-03-10 15:42:23 -0800

    Refactor init with Homepage Hello world

---
+++ bus-frontend/src/components/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Homepage extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Hello world</p>
+      </div>
+    )
+  }
+}
+
+export default Homepage;
