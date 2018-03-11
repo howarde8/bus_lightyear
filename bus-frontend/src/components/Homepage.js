6441c610574995511a1f8cfae470ebb2689b1dd1
howarde8 <howarde8@gmail.com>
2018-03-10 16:23:43 -0800

    Add bootstrap

--- bus-frontend/src/components/Homepage.js
+++ bus-frontend/src/components/Homepage.js
@@ -3,8 +3,13 @@ import React from 'react';
 class Homepage extends React.Component {
   render() {
     return (
-      <div>
-        <p>Hello world</p>
+      <div class="container-fluid">
+        <div class="row">
+            <div>
+              <h1>巴士光年</h1>
+              <h4>最快速且方便的遊覽車預訂體驗</h4>
+            </div>
+        </div>
       </div>
     )
   }
