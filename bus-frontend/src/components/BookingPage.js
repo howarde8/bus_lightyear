5aa4b53932a55e39643f0290b7dffb32a29e34a7
howarde8 <howarde8@gmail.com>
2018-03-19 16:58:00 -0700

    Modify file names p -> P

---
+++ bus-frontend/src/components/BookingPage.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import '../styles/Bookingpage.css';
+
+class Bookingpage extends React.Component {
+  
+  render() {
+    return (
+      <div id="booking_page">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-8 col-xs-12 booking_area"> 
+              <h4 className="booking_id">訂單編號： J578578</h4>
+              <h1 className="bus_name">臺北市雙層巴士</h1>
+            </div>
+            <div className="col-sm-4 col-xs-12 btn_area">  
+              <div className="btn_space">
+                <div className="change_btn btn">更改預訂</div>
+                <div className="print_btn btn">列印</div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm-7 user_area"> 
+              <div className="block">
+                <div className="row">
+                  <div className="title col-xs-4">預訂帳號    </div>
+                  <div className="display user_id col-xs-7">j5630300127</div>
+                </div>
+                <div className="row">
+                  <div className="title col-xs-4">聯絡電話    </div>
+                  <div className="display user_id col-xs-7">+886-911-151-127</div>
+                </div>
+                <hr/>
+                <div className="row">
+                  <div className="title col-xs-4">乘車時間   </div>
+                  <div className="display user_id col-xs-7">2018/02/28(四) 
+                    <div className="display">08:00</div>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="title col-xs-4">結束時間    </div>
+                  <div className="display user_id col-xs-7">2018/02/28(四)
+                    <div className="display">17:00</div>
+                  </div>
+                </div>
+                <hr/>
+                <div className="row">
+                  <div className="title col-xs-4">乘車地址   </div>
+                  <div className="display user_id col-xs-7">新竹縣寶山鄉基八一路87號</div>
+                </div>
+                <hr/>
+                <div className="row">
+                  <div className="title col-xs-4">行程描述  </div>
+                  <div className="display user_id col-xs-7">由寶山雙胞胎井出發 --> 北浦擂茶 --> 南寮腳踏車 --> 新竹火車站 --> 寶山鄉基八一路 </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-5 company_area">
+              <div className="block">
+                <div className="row">
+                  <div className="pic col-sm-12"></div>
+                </div>
+                <div className="row">
+                  <p className="col-sm-12">這裡放公司的簡介，然後限制字數（80）</p>
+                </div>
+                <div className="row">
+                  <div className="btn">查看更多</div>
+                </div>
+                <hr/>
+                <div className="row">
+                  <div className="title col-xs-4">聯絡電話</div>
+                  <div className="display user_id col-xs-7">+886-3567-8111</div>
+                </div>
+                <div className="row">
+                  <div className="title col-xs-4">聯絡信箱</div>
+                  <div className="display user_id col-xs-7">fake_mail@yahoo.com.tw</div>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-12 pic_area">
+              <div className="pic"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Bookingpage;
---
+++ bus-frontend/src/components/HomePage.js
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+import '../styles/Homepage.css';
+
+import Footer from './Footer';
+import Navbar from './Navbar';
+import ItemList from '../containers/ItemList';
+import SearchBar from '../containers/SearchBar';
+import SelectionItem from '../containers/SelectionItem';
+
+class Section1 extends React.Component{
+  render(){
+    return (
+      
+        <div className="row">
+            <div className="picture col-sm-12">    
+            </div>
+            <div className="cover-block">
+            </div>
+            <div className="block">
+                <h1 className="title">巴士光年</h1>
+                <h4 className="slogan">最快速且方便的遊覽車預訂體驗</h4>
+                {/* 搜尋表單 */}
+                <SearchBar/>
+                {/* <form className="navbar-form" role="search"> */}
+                
+                {/* </form> */}
+                
+            </div>
+            </div>
+          
+    
+    );
+  }
+}
+class Section2 extends React.Component{
+  render(){
+    return(
+      <div className="row section2_row">
+        <div className="section2_mainDiv"> 
+          <div className="col-sm-1">
+          <img className="section2_icon" src="https://i.imgur.com/epYOT2a.png" alt=""/>
+          </div>
+          <div className="col-sm-3 section2_subDiv">
+            <h3 className="section2_title">快速搜尋</h3>
+            <h5 className="section2_content">輸入地點或是乘車人數立即找到相關結果</h5>
+          </div>
+          <div className="col-sm-1">      
+          <img className="section2_icon" src="https://i.imgur.com/CrImiud.png" alt=""/>
+          </div>
+          <div className="col-sm-3 section2_subDiv">
+            <h3 className="section2_title"> 資訊透明</h3>
+            <h5 className="section2_content">完整資訊結合用戶評價系統讓您能夠評估最好的選擇</h5>
+          </div>
+          <div className="col-sm-1">
+          <img className="section2_icon" src="https://i.imgur.com/HrYLeiJ.png" alt=""/>
+          </div>
+          <div className="col-sm-3 section2_subDiv">  
+            <h3 className="section2_title">線上預訂</h3>
+            <h5 className="section2_content">取代繁瑣的電話預訂線上即可完成預訂與交易</h5>
+          </div>
+        </div>
+    </div>
+    );
+  }
+}
+class Section3 extends React.Component{
+  render(){
+    return(
+      <div className="row">
+        <div className="col-sm-12">
+          <div className="section3_block">
+            <h1 className="title">{this.props.title}</h1>
+            <h5 className="content">{this.props.content}</h5>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
+
+class Section5 extends React.Component{
+  render(){
+    return(
+      <div className="row bgc-white">
+      <div className="button-area col-sm-4 col-sm-offset-8">
+        <button className="more-button">更多選擇</button>
+      </div>
+    </div>
+    );
+  }
+}
+
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navbar/>
+        <div className="intro" style={{display:"flex",flexDirection:"column"}}>
+          <div className="container-fluid">
+            <Section1 />
+            <Section2 />
+            <Section3 title="明日的旅程，從現在開始" content="點選以下的遊覽車，看看哪些符合您旅行的規格【廣告】"/>
+            {/* <SelectionItem />
+            <ItemList /> */}
+            <Section5 />
+            <Footer />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
+
+
+
+
