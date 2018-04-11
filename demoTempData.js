7c9902480ac73f4791003c77de120a262683da4f
howarde8 <howarde8@gmail.com>
2018-04-11 00:42:04 -0700

    Add demoTempData.js

---
+++ demoTempData.js
@@ -0,0 +1,298 @@
+module.exports = {
+  "bus": [
+    {
+      "id": "5a27db9f734d1d64c8d1973a",
+      "name": "雄獅32人座尊榮巴士",
+      "company_name": "雄獅旅行社股份有限公司",
+      "company_id": "None",
+      "category": "日牌",
+      "brand_name": "HINO-日野",
+      "reg_date": "2017/10",
+      "img_src": "https://i.imgur.com/Uq1OjPI.jpg",
+      "company_src": "https://i.imgur.com/fhUpWzp.png",
+      "description": {
+          "max_amount": 32,
+          "start_time": "08:00",
+          "end_time": "19:00",
+          "introduction": "雄獅通運提供您多元租車服務 :遊覽車包車、港口及機場接送、企業及學校機關單元交通車接駁、會議及會展接駁服務對象包含：企業、機關、社區、社團、宗教團體及家族等客製化包車服務。每一輛車都有專屬的完整資料檔1.車輛基本資料 2.行照資料 3.保險資料 4.公會互助險每一位駕駛，都有專屬的完整資料檔1.駕駛執照 2.駕駛人登記證 3.定期教育訓練 4.良民證 5.無肇事紀錄 6.一年內健康檢查。",
+          "precaution": ["強化玻璃最脆弱的一點，就是玻璃的四個角落，只要用普通鎚子尖的一端敲擊四個角落的任何一個角落，玻璃立即碎裂整片垮下。", "遊覽車為顧及乘客的安全，除了按照規定設置逃生門及安全天窗，另於駕駛座後及車輛中端部分準備有尖型鐵鎚已備不時之需。", "如遇事故 ! 在逃生時讓乘客能夠利用車上預備的尖型鐵鎚，擊破大面積的強化車窗玻璃，是最快的緊急逃生方式。"],
+          "subscription": "最少租用8小時。",
+          "subscription_cancel": "在旅程開始7天前取消預訂，可獲得50%的退款。如果在旅程開始前7天內取消預訂，住宿費用將無法退還。"
+      },
+      "entertainment": ["無線網路", "有線電視", "廁所"],
+      "safety_facility": ["None"],
+      "order": {
+          "availability": true,
+          "price": 8888,
+          "discount": 0.79
+      },
+      "comment": {
+        "stars": 5,
+        "contents": [
+          "搭乘之後覺得價格偏貴，但大致上體驗不錯！",
+          "讚！"
+        ]
+      },
+      "booking": {
+        "serial": "J5630300127",
+        "phone": "+886-911-151-127",
+        "start_time": "2018/02/28(四) 08:00",
+        "end_time": "2018/02/28(四) 19:00",
+        "address": "新竹縣寶山鄉高峰路32號",
+        "description": "由寶山雙胞胎井出發 --> 北浦擂茶 --> 南寮腳踏車 --> 新竹火車站 --> 寶山鄉基八一路"
+      }
+    },
+    {
+      "id": "5a27db9f734d1d64c8d1973a",
+      "name": "雄獅41人座大型巴士",
+      "company_name": "雄獅旅行社股份有限公司",
+      "company_id": "None",
+      "category": "日牌",
+      "brand_name": "TOYOTA-豐田",
+      "reg_date": "2017/10",
+      "img_src": "https://i.imgur.com/0KWrqka.jpg",
+      "company_src": "https://i.imgur.com/fhUpWzp.png",
+      "description": {
+          "max_amount":  41,
+          "start_time": "08:00",
+          "end_time": "19:00",
+          "introduction": "雄獅通運提供您多元租車服務 :遊覽車包車、港口及機場接送、企業及學校機關單元交通車接駁、會議及會展接駁服務對象包含：企業、機關、社區、社團、宗教團體及家族等客製化包車服務。每一輛車都有專屬的完整資料檔1.車輛基本資料 2.行照資料 3.保險資料 4.公會互助險每一位駕駛，都有專屬的完整資料檔1.駕駛執照 2.駕駛人登記證 3.定期教育訓練 4.良民證 5.無肇事紀錄 6.一年內健康檢查。",
+          "precaution": ["強化玻璃最脆弱的一點，就是玻璃的四個角落，只要用普通鎚子尖的一端敲擊四個角落的任何一個角落，玻璃立即碎裂整片垮下。", "遊覽車為顧及乘客的安全，除了按照規定設置逃生門及安全天窗，另於駕駛座後及車輛中端部分準備有尖型鐵鎚已備不時之需。", "如遇事故 ! 在逃生時讓乘客能夠利用車上預備的尖型鐵鎚，擊破大面積的強化車窗玻璃，是最快的緊急逃生方式。"],
+          "subscription": "最少租用3小時。",
+          "subscription_cancel": "在旅程開始7天前取消預訂，可獲得50%的退款。如果在旅程開始前7天內取消預訂，住宿費用將無法退還。"
+      },
+      "entertainment": ["無線網路", "有線電視", "廁所"],
+      "safety_facility": ["None"],
+      "order": {
+          "availability": true,
+          "price": 11000,
+          "discount": 0.79
+      },
+      "comment": {
+        "stars": 5,
+        "contents": [
+          "搭乘之後覺得價格偏貴，但大致上體驗不錯！",
+          "讚！"
+        ]
+      },
+      "booking": {
+        "serial": "J5630300127",
+        "phone": "+886-911-151-127",
+        "start_time": "2018/02/28(四) 08:00",
+        "end_time": "2018/02/28(四) 19:00",
+        "address": "新竹縣寶山鄉高峰路32號",
+        "description": "由寶山雙胞胎井出發 --> 北浦擂茶 --> 南寮腳踏車 --> 新竹火車站 --> 寶山鄉基八一路"
+      }
+    },
+    {
+      "id": "5a27db9f734d1d64c8d1973a",
+      "name": "雄獅20人座中型巴士",
+      "company_name": "雄獅旅行社股份有限公司",
+      "company_id": "None",
+      "category": "未知",
+      "brand_name": "未知",
+      "reg_date": "2017/10",
+      "img_src": "https://i.imgur.com/bvt68cf.jpg",
+      "company_src": "https://i.imgur.com/fhUpWzp.png",
+      "description": {
+          "max_amount": 20,
+          "start_time": "08:00",
+          "end_time": "19:00",
+          "introduction": "雄獅通運提供您多元租車服務 :遊覽車包車、港口及機場接送、企業及學校機關單元交通車接駁、會議及會展接駁服務對象包含：企業、機關、社區、社團、宗教團體及家族等客製化包車服務。每一輛車都有專屬的完整資料檔1.車輛基本資料 2.行照資料 3.保險資料 4.公會互助險每一位駕駛，都有專屬的完整資料檔1.駕駛執照 2.駕駛人登記證 3.定期教育訓練 4.良民證 5.無肇事紀錄 6.一年內健康檢查。",
+          "precaution": ["強化玻璃最脆弱的一點，就是玻璃的四個角落，只要用普通鎚子尖的一端敲擊四個角落的任何一個角落，玻璃立即碎裂整片垮下。", "遊覽車為顧及乘客的安全，除了按照規定設置逃生門及安全天窗，另於駕駛座後及車輛中端部分準備有尖型鐵鎚已備不時之需。", "如遇事故 ! 在逃生時讓乘客能夠利用車上預備的尖型鐵鎚，擊破大面積的強化車窗玻璃，是最快的緊急逃生方式。"],
+          "subscription": "最少租用8小時。",
+          "subscription_cancel": "在旅程開始7天前取消預訂，可獲得50%的退款。如果在旅程開始前7天內取消預訂，住宿費用將無法退還。"
+      },
+      "entertainment": ["無線網路", "有線電視", "廁所"],
+      "safety_facility": ["None"],
+      "order": {
+          "availability": true,
+          "price": 5888,
+          "discount": 0.79
+      },
+      "comment": {
+        "stars": 5,
+        "contents": [
+          "搭乘之後覺得價格偏貴，但大致上體驗不錯！",
+          "讚！"
+        ]
+      },
+      "booking": {
+        "serial": "J5630300127",
+        "phone": "+886-911-151-127",
+        "start_time": "2018/02/28(四) 08:00",
+        "end_time": "2018/02/28(四) 19:00",
+        "address": "新竹縣寶山鄉高峰路32號",
+        "description": "由寶山雙胞胎井出發 --> 北浦擂茶 --> 南寮腳踏車 --> 新竹火車站 --> 寶山鄉基八一路"
+      }
+    },
+    {
+      "id": "5a27db9f734d1d64c8d1973a",
+      "name": "ego42人座大型巴士",
+      "company_name": "e-go 臺灣租車旅遊集團",
+      "company_id": "None",
+      "category": "歐美牌",
+      "brand_name": "ＶＯＬＶＯ",
+      "reg_date": "2017/10",
+      "img_src": "https://i.imgur.com/TnYUrIf.jpg",
+      "company_src": "https://trade.1111.com.tw/Include/getPhoto.ashx?vType=1&vNo=15749",
+      "description": {
+          "max_amount": 42,
+          "start_time": "08:00",
+          "end_time": "19:00",
+          "introduction": "每輛車配有GPS衛星定位系統，控管中心可以隨時掌握車輛的行駛速度、行駛路線等訊息，靈活調派車隊。在政府全面開放大陸客來台之際，本集團不僅添購新車，也增加LED電子顯示螢幕，提供客製化的服務。",
+          "precaution": ["強化玻璃最脆弱的一點，就是玻璃的四個角落，只要用普通鎚子尖的一端敲擊四個角落的任何一個角落，玻璃立即碎裂整片垮下。", "遊覽車為顧及乘客的安全，除了按照規定設置逃生門及安全天窗，另於駕駛座後及車輛中端部分準備有尖型鐵鎚已備不時之需。", "如遇事故 ! 在逃生時讓乘客能夠利用車上預備的尖型鐵鎚，擊破大面積的強化車窗玻璃，是最快的緊急逃生方式。"],
+          "subscription": "最少租用3小時。",
+          "subscription_cancel": "在旅程開始7天前取消預訂，可獲得50%的退款。如果在旅程開始前7天內取消預訂，住宿費用將無法退還。"
+      },
+      "entertainment": ["無線網路", "有線電視", "廁所"],
+      "safety_facility": ["None"],
+      "order": {
+          "availability": true,
+          "price": 12488,
+          "discount": 0.79
+      },
+      "comment": {
+        "stars": 4,
+        "contents": [
+          "搭乘之後覺得價格偏貴，但大致上體驗不錯！",
+          "讚！"
+        ]
+      },
+      "booking": {
+        "serial": "J5630300127",
+        "phone": "+886-911-151-127",
+        "start_time": "2018/02/28(四) 08:00",
+        "end_time": "2018/02/28(四) 19:00",
+        "address": "新竹縣寶山鄉高峰路32號",
+        "description": "由寶山雙胞胎井出發 --> 北浦擂茶 --> 南寮腳踏車 --> 新竹火車站 --> 寶山鄉基八一路"
+      }
+    },
+    {
+      "id": "5a27db9f734d1d64c8d1973a",
+      "name": "ego29人座中型巴士",
+      "company_name": "e-go 臺灣租車旅遊集團",
+      "company_id": "None",
+      "category": "國產",
+      "brand_name": "TOYOTA-豐田",
+      "reg_date": "2017/10",
+      "img_src": "https://i.imgur.com/QczOIVa.png",
+      "company_src": "https://trade.1111.com.tw/Include/getPhoto.ashx?vType=1&vNo=15749",
+      "description": {
+          "max_amount": 29,
+          "start_time": "08:00",
+          "end_time": "19:00",
+          "introduction": "每輛車配有GPS衛星定位系統，控管中心可以隨時掌握車輛的行駛速度、行駛路線等訊息，靈活調派車隊。在政府全面開放大陸客來台之際，本集團不僅添購新車，也增加LED電子顯示螢幕，提供客製化的服務。",
+          "precaution": ["強化玻璃最脆弱的一點，就是玻璃的四個角落，只要用普通鎚子尖的一端敲擊四個角落的任何一個角落，玻璃立即碎裂整片垮下。", "遊覽車為顧及乘客的安全，除了按照規定設置逃生門及安全天窗，另於駕駛座後及車輛中端部分準備有尖型鐵鎚已備不時之需。", "如遇事故 ! 在逃生時讓乘客能夠利用車上預備的尖型鐵鎚，擊破大面積的強化車窗玻璃，是最快的緊急逃生方式。"],
+          "subscription": "最少租用3小時。",
+          "subscription_cancel": "在旅程開始7天前取消預訂，可獲得50%的退款。如果在旅程開始前7天內取消預訂，住宿費用將無法退還。"
+      },
+      "entertainment": ["無線網路", "有線電視", "廁所"],
+      "safety_facility": ["None"],
+      "order": {
+          "availability": true,
+          "price": 8500,
+          "discount": 0.79
+      },
+      "comment": {
+        "stars": 4,
+        "contents": [
+          "搭乘之後覺得價格偏貴，但大致上體驗不錯！",
+          "讚！"
+        ]
+      },
+      "booking": {
+        "serial": "J5630300127",
+        "phone": "+886-911-151-127",
+        "start_time": "2018/02/28(四) 08:00",
+        "end_time": "2018/02/28(四) 19:00",
+        "address": "新竹縣寶山鄉高峰路32號",
+        "description": "由寶山雙胞胎井出發 --> 北浦擂茶 --> 南寮腳踏車 --> 新竹火車站 --> 寶山鄉基八一路"
+      }
+    },
+    {
+      "id": "5a27db9f734d1d64c8d1973a",
+      "name": "東南43人座大客車",
+      "company_name": "東南遊覽汽車股份有限",
+      "company_id": "None",
+      "category": "歐美牌",
+      "brand_name": "瑞典Scania",
+      "reg_date": "2017/10",
+      "img_src": "https://i.imgur.com/Xbt0506.jpg",
+      "company_src": "https://i.imgur.com/OHyVLwb.jpg",
+      "description": {
+          "max_amount": 43,
+          "start_time": "08:00",
+          "end_time": "19:00",
+          "introduction": "東南遊覽汽車公司所屬之車輛均為歐洲進口之瑞典SCANIA之車系，此車種在安全的結構上，剎車系統多加了整套電磁剎車系統，底盤可昇降氣壓式系統，更擁有最高級的氣墊式避震系統，有以上的系統，更安全、且舒適、又平穩。",
+          "precaution": ["強化玻璃最脆弱的一點，就是玻璃的四個角落，只要用普通鎚子尖的一端敲擊四個角落的任何一個角落，玻璃立即碎裂整片垮下。", "遊覽車為顧及乘客的安全，除了按照規定設置逃生門及安全天窗，另於駕駛座後及車輛中端部分準備有尖型鐵鎚已備不時之需。", "如遇事故 ! 在逃生時讓乘客能夠利用車上預備的尖型鐵鎚，擊破大面積的強化車窗玻璃，是最快的緊急逃生方式。"],
+          "subscription": "最少租用8小時。",
+          "subscription_cancel": "在旅程開始7天前取消預訂，可獲得50%的退款。如果在旅程開始前7天內取消預訂，住宿費用將無法退還。"
+      },
+      "entertainment": ["無線網路", "有線電視", "廁所"],
+      "safety_facility": ["None"],
+      "order": {
+          "availability": true,
+          "price": 11888,
+          "discount": 0.79
+      },
+      "comment": {
+        "stars": 5,
+        "contents": [
+          "搭乘之後覺得價格偏貴，但大致上體驗不錯！",
+          "讚！"
+        ]
+      },
+      "booking": {
+        "serial": "J5630300127",
+        "phone": "+886-911-151-127",
+        "start_time": "2018/02/28(四) 08:00",
+        "end_time": "2018/02/28(四) 19:00",
+        "address": "新竹縣寶山鄉高峰路32號",
+        "description": "由寶山雙胞胎井出發 --> 北浦擂茶 --> 南寮腳踏車 --> 新竹火車站 --> 寶山鄉基八一路"
+      }
+    },
+    {
+      "id": "5a27db9f734d1d64c8d1973a",
+      "name": "東南30人座中客車",
+      "company_name": "東南遊覽汽車股份有限",
+      "company_id": "None",
+      "category": "歐美牌",
+      "brand_name": "瑞典Scania",
+      "reg_date": "2017/10",
+      "img_src": "https://i.imgur.com/kU9qojo.jpg",
+      "company_src": "https://i.imgur.com/OHyVLwb.jpg",
+      "description": {
+          "max_amount": 30,
+          "start_time": "08:00",
+          "end_time": "19:00",
+          "introduction": "東南遊覽汽車公司所屬之車輛均為歐洲進口之瑞典SCANIA之車系，此車種在安全的結構上，剎車系統多加了整套電磁剎車系統，底盤可昇降氣壓式系統，更擁有最高級的氣墊式避震系統，有以上的系統，更安全、且舒適、又平穩。",
+          "precaution": ["強化玻璃最脆弱的一點，就是玻璃的四個角落，只要用普通鎚子尖的一端敲擊四個角落的任何一個角落，玻璃立即碎裂整片垮下。", "遊覽車為顧及乘客的安全，除了按照規定設置逃生門及安全天窗，另於駕駛座後及車輛中端部分準備有尖型鐵鎚已備不時之需。", "如遇事故 ! 在逃生時讓乘客能夠利用車上預備的尖型鐵鎚，擊破大面積的強化車窗玻璃，是最快的緊急逃生方式。"],
+          "subscription": "最少租用8小時。",
+          "subscription_cancel": "在旅程開始7天前取消預訂，可獲得50%的退款。如果在旅程開始前7天內取消預訂，住宿費用將無法退還。"
+      },
+      "entertainment": ["無線網路", "有線電視", "廁所"],
+      "safety_facility": ["None"],
+      "order": {
+          "availability": true,
+          "price": 8888,
+          "discount": 0.79
+      },
+      "comment": {
+        "stars": 5,
+        "contents": [
+          "搭乘之後覺得價格偏貴，但大致上體驗不錯！",
+          "讚！"
+        ]
+      },
+      "booking": {
+        "serial": "J5630300127",
+        "phone": "+886-911-151-127",
+        "start_time": "2018/02/28(四) 08:00",
+        "end_time": "2018/02/28(四) 19:00",
+        "address": "新竹縣寶山鄉高峰路32號",
+        "description": "由寶山雙胞胎井出發 --> 北浦擂茶 --> 南寮腳踏車 --> 新竹火車站 --> 寶山鄉基八一路"
+      }
+    }
+  ]
+}
