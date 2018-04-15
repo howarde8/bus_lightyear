28ed15010aff6db4d664c0e6873816cdafbf14ea
Louis <pohsiu0709@gmail.com>
2018-04-15 09:01:40 +0800

    remove useless

--- bus-react/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux-immutable';
-import products from './products';
-import visibilityFilter from './visibilityFilter';
-const reducers = combineReducers({
-    products,
-    visibilityFilter,
-})
-
-export default reducers
