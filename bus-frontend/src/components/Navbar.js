716ac8f8d864c27ebe94bb5e2cb7817a1074b91e
Louis <pohsiu0709@gmail.com>
2018-04-22 09:55:11 +0800

    simple modal

--- bus-frontend/src/components/Navbar.js
+++ bus-frontend/src/components/Navbar.js
@@ -1,5 +1,51 @@
 import React from 'react';
+import Modal from '../components/Modal';
 
+class LoginModal extends React.Component{
+    render(){
+        const children = (
+            <div class="login_cover_off">
+                <div class="login_cover"></div>
+                <div class="login_window">
+                    <div class="close_window"> 
+                        <div class="text">Ｘ</div>
+                    </div>
+                    <div class="facebook_login button"> 
+                        <div class="icon"></div>
+                        <div class="text">使用facebook帳號登入</div>
+                    </div>
+                    <div class="google_login button"> 
+                        <div class="icon"></div>
+                        <div class="text">使用Google帳號登入</div>
+                    </div>
+                    <div class="text_or">————————— 或 —————————</div>
+                    <div class="email_input button"> 
+                        <input placeholder="電子郵件地址"/>
+                    </div>
+                        <div class="password_input button"> 
+                        <input type="password" placeholder="密碼"/>
+                        <div class="icon"></div>
+                    </div>
+                    <div class="login_forgot">忘記密碼？</div>
+                    <div class="login_button button"> 
+                        <div class="text">登入</div>
+                    </div>
+                    <hr/>
+                    <h4>還沒有帳號嗎？</h4>
+                    <div class="change_to_signup"> 
+                        <div class="text">註冊 </div>
+                    </div>
+                </div>
+            </div>
+        )
+        return(
+            <Modal
+                title={"登入"}
+                children={children}
+            />
+        )
+    }
+}
 
 const Navbar = () => {
     return(
@@ -28,7 +74,8 @@ const Navbar = () => {
             <li><a href="#">刊登資料</a></li>
             <li><a href="./backend">後台管理</a></li>
             <li><a href="#">臉書粉專</a></li>
-            <li><a href="#">登入</a></li>
+            <li><a href="#"><LoginModal/></a></li>
+            {/* <li><a href="#">登入</a></li> */}
             <li><a href="#">註冊</a></li>
         </ul>
 
