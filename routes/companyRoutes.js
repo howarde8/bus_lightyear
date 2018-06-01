5767799cf585c25cb3d33d6fb65deec159203fbf
howarde8 <howarde8@gmail.com>
2018-05-31 22:20:35 -0700

    Add company APIs

--- routes/companyRoutes.js
+++ routes/companyRoutes.js
@@ -1,10 +1,22 @@
 const mongoose = require("mongoose");
-const requireLogin = require('../middlewares/requireLogin');
+const requireLogin = require("../middlewares/requireLogin");
 
-const Company = mongoose.model('companies');
+const Company = mongoose.model("companies");
 
 module.exports = app => {
-  app.post('/api/company/add', requireLogin, (req, res) => {
+  app.get("/api/company/all", (req, res) => {
+    Company.find().then(companies => {
+      res.send(companies);
+    });
+  });
+
+  app.get("/api/company/:id", (req, res) => {
+    Company.findById(req.params.id).then(company => {
+      res.send(company);
+    })
+  });
+
+  app.post("/api/company/add", requireLogin, (req, res) => {
     const { name, phone, address, introduction, buses, users } = req.body;
 
     const company = new Company({
@@ -18,15 +30,40 @@ module.exports = app => {
     });
 
     Company.findOne({ name }).then(existingCompany => {
-      console.log('out', existingCompany);
       if (existingCompany) {
-        console.log('exist');
-        res.status(403).send({ error: 'Company exists!' });
+        res.status(403).send({ error: "Company exists!" });
       } else {
-        console.log('not exist');
-        company
-          .save()
-          .then(company => { res.send(company); });
+        company.save().then(company => {
+          res.send(company);
+        });
+      }
+    });
+  });
+
+  app.post("/api/company/update", requireLogin, (req, res) => {
+    const { id, name, phone, address, introduction, buses, users } = req.body;
+
+    // find exist field
+    var company = { name, phone, address, introduction, buses, users };
+    for (key in company) {
+      if (!company[key]) delete company[key];
+    }
+
+    Company.findByIdAndUpdate(id, company).then(existingCompany => {
+      if (existingCompany) {
+        res.send(existingCompany);
+      } else {
+        res.status(403).send({ error: "Company not exists!" });
+      }
+    });
+  });
+
+  app.post("/api/company/delete", requireLogin, (req, res) => {
+    Company.findByIdAndRemove(req.body.id).then(existingCompany => {
+      if (existingCompany) {
+        res.send(existingCompany);
+      } else {
+        res.status(403).send({ error: "Company not exists!" });
       }
     });
   });
